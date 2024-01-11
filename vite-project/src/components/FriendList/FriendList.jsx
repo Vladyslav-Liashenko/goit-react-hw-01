c52e18b7dbc81822a9996422aeaca584d456f5d0
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2024-01-11 22:48:31 +0200

    save 2

--- vite-project/src/components/FriendList/FriendList.jsx
+++ vite-project/src/components/FriendList/FriendList.jsx
@@ -1,17 +1,18 @@
 import { FriendListItem } from './FriendListItem/FriendListItem';
 
 export const FriendList = ({ friends }) => {
-  console.log(friends);
   return (
     <div>
       <ul className="friend-list">
-        <li>
-          <FriendListItem />
-          <FriendListItem />
-          <FriendListItem />
-          <FriendListItem />
-          <FriendListItem />
-        </li>
+        {friends.map(el => (
+          <li key={el.id}>
+            <FriendListItem
+              isOnline={el.isOnline}
+              avatar={el.avatar}
+              name={el.name}
+            />
+          </li>
+        ))}
       </ul>
     </div>
   );
--- vite-project/src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ vite-project/src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -1,9 +1,9 @@
-export const FriendListItem = () => {
+export const FriendListItem = ({ isOnline, name, avatar }) => {
   return (
     <div className="item">
-      <img className="avatar" src="" alt="Avatar" width="48" />
-      <p className="name">Friend name</p>
-      <p className="status">Friend status</p>
+      <img className="avatar" src={avatar} alt="Avatar" width="48" />
+      <p className="name">{name}</p>
+      <p className="status">{isOnline ? 'Online' : 'Offline'}</p>
     </div>
   );
 };
