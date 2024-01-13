efe69af9b93b17de1f70d8deac5f2cd4569a0d45
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2024-01-13 18:12:14 +0200

    save

--- goit-react-hw-01 17.57.56/src/components/FriendList/FriendListItem/FriendListItem.jsx
+++
@@ -1,9 +0,0 @@
-export const FriendListItem = ({ isOnline, name, avatar }) => {
-  return (
-    <div className="item">
-      <img className="avatar" src={avatar} alt="Avatar" width="48" />
-      <p className="name">{name}</p>
-      <p className="status">{isOnline ? 'Online' : 'Offline'}</p>
-    </div>
-  );
-};
