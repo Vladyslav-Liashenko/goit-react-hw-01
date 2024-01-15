74d9e19939aa3adfa8b567efa98b8cd9d2f38fc8
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2024-01-15 11:36:17 +0200

    save

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -1,18 +1,21 @@
-import styles from'../FriendList.module.css'
+import styles from '../FriendList.module.css';
+
 export const FriendListItem = ({ isOnline, name, avatar }) => {
 
   const statusClasses = [styles.status];
   if (isOnline) {
     statusClasses.push(styles.is_active);
-  }else {
+  } else {
     statusClasses.push(styles.is_retired);
   }
-  
+
+  console.log(statusClasses.join(' '));
+
   return (
     <div className={styles.item}>
       <img className={styles.avatar} src={avatar} alt="Avatar" width="108" />
       <p className={styles.name_friend}>{name}</p>
-      <p className={statusClasses.join(' ')}>{isOnline ? styles.Online : styles.Offline}</p>
+      <p className={statusClasses.join(' ')}>{isOnline ? 'Online' : 'Offline'}</p>
     </div>
   );
 };
